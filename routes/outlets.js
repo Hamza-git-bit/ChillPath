b09b84d6599eb813608e1e71c114f03a7ec9eae6
Hamza-git-bit <mhamzash000@gmail.com>
2025-07-09 06:39:21 +0000

    Refactor routes/outlets.js to Use Shared File Storage

--- routes/outlets.js
+++ routes/outlets.js
@@ -1,20 +1,6 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
+const { loadData, saveData } = require('../utils/fileStorage');
 const router = express.Router();
-
-const filePath = path.join(__dirname, '../data/outlets.json');
-
-// Helpers
-function loadOutlets() {
-  if (!fs.existsSync(filePath)) return [];
-  const data = fs.readFileSync(filePath, 'utf8');
-  return JSON.parse(data);
-}
-
-function saveOutlets(outlets) {
-  fs.writeFileSync(filePath, JSON.stringify(outlets, null, 2));
-}
 
 // GET /api/outlets
 router.get('/', (req, res) => {
@@ -30,7 +16,7 @@ router.post('/', (req, res) => {
     return res.status(400).json({ message: 'Name and contact required.' });
   }
 
-  const outlets = loadOutlets();
+  const outlets = loadData('outlets.json');
 
   const newOutlet = {
     id: outlets.length > 0 ? outlets[outlets.length - 1].id + 1 : 1,
@@ -39,7 +25,7 @@ router.post('/', (req, res) => {
   };
 
   outlets.push(newOutlet);
-  saveOutlets(outlets);
+  saveData('outlets.json', outlets);
 
   res.status(201).json({ message: 'Outlet added', outlet: newOutlet });
 });
