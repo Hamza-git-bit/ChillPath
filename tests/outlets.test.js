0b9b3d07c87c67bc66bb3d2a93c16a9a83ac5668
Hamza-git-bit <mhamzash000@gmail.com>
2025-07-13 19:15:54 +0000

    Create placeholder test files

---
+++ tests/outlets.test.js
@@ -0,0 +1,6 @@
+// tests/outlets.test.js
+describe('Outlets API', () => {
+  it('should pass a dummy test', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ tests/products.test.js
@@ -0,0 +1,6 @@
+// tests/products.test.js
+describe('Products API', () => {
+  it('should pass a dummy test', () => {
+    expect(true).toBe(true);
+  });
+});
